8662df02e62436669c1a269cdcfd1b866b2f766a
Michael Vallido <vallido.michael@gmail.com>
2026-03-05 18:31:24 -0800

    task: setting up hono in data-service

---
+++ apps/data-service/src/hono/app.ts
@@ -0,0 +1,10 @@
+import { Hono } from 'hono';
+
+export const App = new Hono<{ Bindings: Env }>();
+
+App.get('/:id', async (c) => {
+	return c.json({
+		message: 'Hello World!'
+	})
+})
+
--- apps/data-service/src/index.ts
+++ apps/data-service/src/index.ts
@@ -1,7 +1,9 @@
 import { WorkerEntrypoint } from 'cloudflare:workers';
+import { App } from './hono/app';
 
 export default class DataService extends WorkerEntrypoint<Env> {
 	fetch(request: Request) {
-		return new Response('Hello World!');
+		return App.fetch(request, this.env, this.ctx)
 	}
 }
+
