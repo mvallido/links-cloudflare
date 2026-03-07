e1433893ae6f02e84edc1110f45798c305fbf2bc
Michael Vallido <vallido.michael@gmail.com>
2026-03-07 00:08:42 -0800

    feat: save ai evaluation into d1

--- apps/data-service/src/workflows/destination-evaluation-workflow.ts
+++ apps/data-service/src/workflows/destination-evaluation-workflow.ts
@@ -1,9 +1,12 @@
 import { WorkflowEntrypoint, WorkflowEvent, WorkflowStep } from 'cloudflare:workers';
 import { collectDestinationInfo } from '@/helpers/browser-render';
 import { aiDestinationChecker } from '@/helpers/ai-destination-checker';
+import { addEvaluation } from '@repo/data-ops/queries/evaluations';
+import { initDatabase } from '@repo/data-ops/database';
 
 export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, DestinationStatusEvaluationParams> {
 	async run(event: Readonly<WorkflowEvent<DestinationStatusEvaluationParams>>, step: WorkflowStep) {
+		initDatabase(this.env.DB)
 
 		const collectedData = await step.do("Collect rendered destination page data", async () => {
 			return collectDestinationInfo(this.env, event.payload.destinationUrl);
@@ -22,7 +25,15 @@ export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, Desti
 			},
 		);
 
-		console.log(collectedData);
+		const evaluationId = await step.do('Save evaluation in database', async () => {
+			return await addEvaluation({
+				linkId: event.payload.linkId,
+				status: aiStatus.status,
+				reason: aiStatus.statusReason,
+				accountId: event.payload.accountId,
+				destinationUrl: event.payload.destinationUrl,
+			});
+		});
 	}
 }
 
